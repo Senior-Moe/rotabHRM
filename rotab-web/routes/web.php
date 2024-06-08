3e9667294535c6e5d70d6db444c53e9a511d0240
Mohammed-Alnahdi <m.kh.alnahdi@gmail.com>
2024-06-08 06:39:52 +0300

    create adding department and new branch.

--- rotab-web/routes/web.php
+++ rotab-web/routes/web.php
@@ -12,6 +12,17 @@ Route::get('/', function () {
 Route::get('adding_new_employee' , [add_employee_controller::class , 'new_employee']);
 Route::get('adding_new_employee_passport' , [add_employee_passport_controller::class , 'new_adding_passport']);
 Route::get('adding_employee_contact_details',[add_employee_contact_details_controller::class , 'adding_new_contact'] );
+//need make control
 Route::get('company', function(){
     return view('create_company_details');
+});
+Route::get('department', function(){
+    return view('create_department_details');
+});
+Route::get('Branch', function(){
+    return view('create_new_branch');
+});
+
+Route::get('Department', function(){
+    return view('add_new_department');
 });
