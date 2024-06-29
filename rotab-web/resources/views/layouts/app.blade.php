d7b65f377aa22e7598dc8b03cc03e884c477d320
Mohammed-Alnahdi <m.kh.alnahdi@gmail.com>
2024-06-29 03:53:41 +0300

    adding welcome page

--- rotab-web/resources/views/layouts/app.blade.php
+++ rotab-web/resources/views/layouts/app.blade.php
@@ -7,15 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}|@yield('title')</title>
-
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!--Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -25,8 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-
-                    <img src="/images/logo_nav_bar.png"  height="30" class="d-inline-block align-top" alt="Logo">
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -55,7 +50,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
@@ -81,12 +76,5 @@
             @yield('content')
         </main>
     </div>
-<!-- Bootstrap JS and dependencies -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
 </body>
 </html>
--- rotab-web/resources/views/welcome.blade.php
+++ rotab-web/resources/views/welcome.blade.php
@@ -2,9 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <title>Rotab HRM</title>
+        <title>Rotab HRM | </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,6 +13,9 @@
 
         <!-- Styles -->
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="style/style.css" rel="stylesheet"/>
+        <link href="style/responsive.css" rel="stylesheet"/>
     </head>
     <body>
 
@@ -49,8 +53,166 @@
         </ul>
     </div>
 </nav>
+  <!-- slider section -->
+  <section class="slider_section position-relative">
+    <div class="slider_bg_box">
+      <img src="images/slider-bg-1.jpg" alt="">
+    </div>
+    <div class="slider_bg"></div>
+    <div class="container">
+      <div class="col-md-6 ml-auto">
+        <div class="detail-box">
+          <h1>Welcome To <br>Rotab HRM</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda obcaecati fuga nulla ipsa dolores quisquam at illum ipsam rerum. Officia odio exercitationem illum deserunt! Veritatis harum similique autem corrupti laboriosam!
 
-<section style="height: 100vh; background-color: red;"></section>
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End of slider section -->
+
+  <!-- Special section -->
+  <section class="spcl_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="detail_container">
+            <div class="detail-box">
+              <img src="images/opensourcesblue.png" alt="logo of open source but blue">
+              <h5>Open Source</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolores qui quisrum vel ad voluptates vero earum quos esse
+
+              </p>
+            </div>
+            <div class="detail-box">
+              <img src="images/gearblue.png" alt="customization icon">
+              <h5>Customize</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In blanditiis accusanfugit recusandae itaque fugiat ex praesentium,
+
+              </p>
+            </div>
+            <div class="detail-box">
+              <img src="images/License_icon-gpl-generalblue.png" alt="">
+              <h5>GPLv3 License</h5>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate eveniet consectetur quasi perspiciatis modi consequuntur quasi perspiciatis modi consequuntur
+
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-5 ml-auto">
+          <div class="img-box">
+            <img src="images/banner1.jpg" alt="">
+          </div> </div>
+      </div>
+    </div>
+  </section>
+  <!-- End of special section -->
+
+  <!-- about section -->
+  <section class="about_section layout_padding-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/banner2.jpg" alt="">
+
+          </div>
+        </div>
+        <div class="col-md-6"><div class="detail-box">
+          <div class="heading_container">
+            <h2>About Us</h2>
+          </div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam cum consequuntur aliquid, asperiores accusamus, rem explicabo perferendis, dolor repellat numquam adipisci esse. Nobis quisquam, recusandae iusto perferendis porro libero vel.
+
+          </p>
+          <a href="">Read More</a>
+        </div></div>
+      </div>
+    </div>
+  </section>
+<!-- End of about us page -->
+
+
+<!-- Testimonial section -->
+  <section class="client_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>About Programmers</h2>
+      </div>
+      <div class="carousel slide" id="carouselExampleControls" data-ride:="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/client.jpg" alt="">
+              </div>
+              <div class="detail-box">
+                <h4>Mohammed Alnahdi</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit nulla nemo voluptates nisi voluptatibus ullam similique tempore sapiente sint autem ratione ex nihil cupiditate exercitationem, quo, ipsa modi? Autem, veniam!
+
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel_btn-box">
+          <a href="#carouselExampleControls" class="carousel-control-prev" role="button" data-slide="prev">
+            <i class="fa fa-angle-left" aria-hidden="true"></i>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a href="#carouselExampleControls" class="carousel-control-next" role="button" data-slide="next">
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end of client section -->
+
+  <!-- start of footer -->
+  <footer class="footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 footer-col">
+          <div class="footer_content">
+            <h4>{{__('Contact Us')}}</h4>
+            <div class="contact_link_box">
+              <a href=""><i class="fa fa-envelop" aria-hidden="true"></i>
+              <span>m.kh.alnahdi@gmail.com</span></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+          <div class="footer_detail">
+            <a href="" class="footer-logo">
+              {{ __('Rotab HRM') }}
+            </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  excepturi corporis nisi magnam quod ullam magni dolorum.
+
+            </p>
+            <div class="footer_social">
+              <a href=""><i class="fa-brands fa-facebook" aria-hidden="true"></i></a>
+              <a href=""><i class="fa-brands fa-twitter" aria-hidden="true"></i></a>
+              <a href=""><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
+              <a href=""><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+              <a href=""><i class="fa-brands fa-pinterest" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+            <h5>hello World</h5>
+        </div>
+      </div>
+      <div class="footer-info">
+        <p>&copy; <span id="displayYear"></span>All Rights Reserved By
+        <a href="#">GPLv3</a></p>
+      </div>
+    </div>
+  </footer>
 <!-- Bootstrap JS and dependencies -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
